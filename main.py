037890ff9a8d5eb15c326b8076991add9fc0e0c1
osiakmikolaj <osiakmikolaj@gmail.com>
2025-03-22 12:18:30 +0100

    path via sys.args and error handling

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import *
 
 chars = [
@@ -14,8 +15,8 @@ def get_book_text(path):
     try:
         f = open(path)
     except FileNotFoundError:
-        print("File not found!")
-        quit()
+        print("-------- File not found! --------")
+        sys.exit(1)
     else:
         print(f"Analyzing book found at {path}")
         with open(path, 'r') as f:
@@ -23,8 +24,12 @@ def get_book_text(path):
             return file_contents
 
 def main():
+    if len(sys.argv) > 1:
+        text = get_book_text(sys.argv[1])
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     print("============ BOOKBOT ============")
-    text = get_book_text('books/frankenstein.txt')
     print(get_num_words(text))
     print(char_count(text, chars))
     print("============= END ===============")
