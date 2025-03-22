2f28138de83d8d2fa9caae22a426c729518fabe4
osiakmikolaj <osiakmikolaj@gmail.com>
2025-03-22 12:01:23 +0100

    New report look and char count function

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from stats import get_num_words
+from stats import *
+
+chars = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+    'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', 
+    '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', 
+    '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', 
+    '[', ']', '{', '}', ';', ':', '\'', '\"', ',', '.', 
+    '<', '>', '/', '?', '\\', '|', '`', '~', ' '
+]
 
 def get_book_text(path):
-    with open(path, 'r') as f:
-        file_contents = f.read()
-        return file_contents
+    try:
+        f = open(path)
+    except FileNotFoundError:
+        print("File not found!")
+        quit()
+    else:
+        print(f"Analyzing book found at {path}")
+        with open(path, 'r') as f:
+            file_contents = f.read()
+            return file_contents
 
 def main():
-    print(get_num_words(get_book_text('./books/frankenstein.txt')))
+    print("============ BOOKBOT ============")
+    text = get_book_text('books/frankenstein.txt')
+    print(get_num_words(text))
+    print(char_count(text, chars))
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,21 @@
 def get_num_words(text):
-    words = text.split()
-    return f"{len(words)} words found in the document"
+    print("----------- Word Count ----------")
+    return f"Found {len(text.split())} total words"
 
-def char_count(text):
-    pass
+def char_count(text, char_dict):
+    print("--------- Character Count -------")
+    char_count_dict = {}
+    report_text = ""
+
+    for char in char_dict:
+        char_count_dict[char] = text.lower().count(char)
+    
+    char_count_dict = sorted(char_count_dict.items(), key=lambda item: item[1], reverse=True)
+
+    for dict in char_count_dict:
+        if dict == char_count_dict[-1]:
+            report_text += f"{dict[0]}: {dict[1]}"
+        else:
+            report_text += f"{dict[0]}: {dict[1]}\n"
+    
+    return report_text
